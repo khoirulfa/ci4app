2c5d4c56798e7142fcd7e61635dfe95ea590676b
kuuhaku31 <mageart567@gmail.com>
2020-10-30 13:21:39 +0700

    change about page

--- app/Controllers/Pages.php
+++ app/Controllers/Pages.php
@@ -19,9 +19,11 @@ class Pages extends BaseController
         $data = [
             'title' => "About Me | CodeIgniter4",
             'nama' => "A. Musafir Khoirul Fattah",
-            'telp' => "http://wa.me/62895365016420",
-            'insta' => "https://www.instagram.com/muss.kfc/?hl=id",
-            'fb' => "https://www.facebook.com/sndv.20"
+            'wa' => "http://wa.me/62895365016420",
+            'phone' => "+62 895365016420",
+            'instagram' => "https://www.instagram.com/m.khoirul/?hl=id",
+            'fb' => "https://www.facebook.com/sndv.20",
+            'github' => "https://www.facebook.com/sndv.20",
         ];
         return view('pages/about', $data);
     }
